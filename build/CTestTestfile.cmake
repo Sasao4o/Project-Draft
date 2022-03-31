4100886e9135ce0b46167b419577e08a7907573b
Mostafa <sasa1x20001@gmail.com>
2022-03-31 06:56:54 +0200

    Config File Avail Now

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sasa/bustub
+# Build directory: /home/sasa/bustub/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest-build")
+subdirs("src")
+subdirs("test")
---
+++ build/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sasa/bustub/build/googletest-src
+# Build directory: /home/sasa/bustub/build/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
