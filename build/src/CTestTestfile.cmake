86cd362c43fe6f97df2bd57ae9c86839c63f5b88
Ahmed275-Wael <aquaticsir.2001@gmail.com>
2022-03-31 07:17:09 +0200

    x

---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/bustub/src
+# Build directory: /home/ahmed/bustub/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -51,21 +51,22 @@ bool BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) {
   // Make sure you call DiskManager::WritePage!
   //Put All Changes Back To Disk 
     //We Have To Check If If Page Is Modified Or Not !
-    page *p=&pages_[page_table[page_id]]
-	    if (p->isdirty){
-	     diskmanager->WritePage(p->page->id,p->GetData());
-             p->isdirty=false;
-             // u didn't even find the page (.end() act as NULL char)
-	 if (page_table.find(page_id)==page_table.end()) {
-	     return false;
-		  }
+    page *p=&pages_[page_table_[page_id]]
+	     // u didn't even find the page (.end() act as NULL char)
+	     if (page_table_.find(page_id)==page_table_.end()) {
+		                  return false;
+	     }
+	    if (p->isdirty_){
+	     diskmanager->WritePage(p->page_id_,p->GetData());
+             p->isdirty_=false;
+	    } 
 	   return true;
 }
 
 void BufferPoolManagerInstance::FlushAllPgsImp() {
   // We Have To Loop Over Buffer Pool
    for (int i = 0; i < pool_size; i++) {
-	FlushPgImp(pages_[i]->pageid);
+	FlushPgImp(pages_[i]->pageid_);
    }
 }
 
@@ -75,28 +76,93 @@ Page *BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) {
   // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
   // 3.   Update P's metadata, zero out memory and add P to the page table.
   // 4.   Set the page ID output parameter. Return a pointer to P.
-     if (
-  return nullptr;
+        frame_id_t frame;
+	if (free_list_.empty()){
+           if(!replacer_.Victim(&frame){
+		     return nullptr;
+		     
+		     } 
+		     page *oldpage =&pages_[frame];
+		     FlushPgImp(oldpage->page_id_);
+		    
+          }else{
+     frame=free_list_.front();
+     free_list_.pop_front();
+     
+     }
+    	        *page_id=AllocatePage();
+                 pages_[frame].page_id_=*page_id;
+	         page_table_[*page_id]=frame;
+	         pages_[frame].ResetMemory();
+	         replacer_->Pin(frame);
+	         pages[frame].pin_count_=1;
+		 pages[frame].is_dirty_=_false;
+  return &pages_[frame];
 }
 
 Page *BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) {
   // 1.     Search the page table for the requested page (P).
   // 1.1    If P exists, pin it and return it immediately.
-  // 1.2    If P does not exist, find a replacement page (R) from either the free list or the replacer.
+  // 1.2    If P dofind a replacement page (R) from either the free list or the replacer.
   //        Note that pages are always found from the free list first.
   // 2.     If R is dirty, write it back to the disk.
   // 3.     Delete R from the page table and insert P.
-  // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-  return nullptr;
+  // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to Preturn nullp.
+ 
+	  	//In Case i Find It In Memory No Need To Go To Disk
+		if (page_table_[page_id] != page_table_.end()) {
+				//In Case i Find It In Memory No Need To Go To Disk
+					replacer_.pin(page_table_[page_id]);
+					            Page *p = &pages_[page_table_[page_id]];
+					                        p.pincount++;
+					                        	return &pages_[page_table_[page_id]];
+					                        	}
+					                        	frame_id_t myFrame;
+					                        	if (free_list_.empty()) {
+					                        		//Go To Victim
+					                        		  if(replacer_.Victim(&myFrame)){
+					                        		           FlushPgImp(pages_[myFrame]);
+					                        		           }else{
+					                        		           return nullptr;
+					                        		           }
+					                        		           } else {
+					                        		           	//Bring It
+					                        		           	replacer_.pin(page_table_[page_id]);
+					                        		           	    replacer_.pincount++;
+					                        		           	        myFrame = free_list_.pop();
+					                        		           	        }
+			                        		           	            Page *p = &pages_[myFrame]
+					                        		           	                char *data;
+					                        		           	                    ReadPage(page_id, data);
+					                        		           	                        p->data_=data;
+					                        		           	                            replacer_.pin(page_table_[page_id]);
+					                        		           	                                p->pincount++;
+					                        		           	                                    page_table_.erase(page_id);
+					                        		           	                                        pages_[myFrame].ResetMemory();
+					                        		           	                                            page_table_[page_id] = myFrame;
+					                        		           	                                                return p;
+				
 }
 
 bool BufferPoolManagerInstance::DeletePgImp(page_id_t page_id) {
   // 0.   Make sure you call DeallocatePage!
-  // 1.   Search the page table for the requested page (P).
+  // 1.   Search the page tabfor the requested page (P).
   // 1.   If P does not exist, return true.
   // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
-  return false;
+  frame_id_t myFrame = page_table_[page_id];
+  Page *trash =&pages_[myFrame];
+   if(page_table_[page_id] == page_table.end()) {
+	   	//Oh No Need To Delete Its Logically Not There
+			return true;
+			}
+			if (trash->GetPinCount()<1){
+			free_list_.emplace_back(myFrame);
+			pages_[page_table_[page_id]].ResetMemory();
+		page_table_.erase(page_id);
+		
+		}
+			return false;
 }
 
 bool BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) { return false; }
